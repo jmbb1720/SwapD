122f42812372db47cfdf53e8dc6b0bbe3f0d922c
Anthony Vo <anthonyhvo12@gmail.com>
2020-08-09 16:13:57 +0700

    create db request handlers

--- public/js/loadPosts.js
+++ public/js/loadPosts.js
@@ -1,3 +1,10 @@
+/**
+Get all posts from the database
+TODO: limit the number of retrieved posts to the last 10-15 only? 
+[give user a load more option to get older posts]
+@param id post id
+@param updates a JSON object consisting of fields and values to update
+ */
 const retrievePosts = async () => {
     try {
         const response = await fetch('/posts',
@@ -13,6 +20,69 @@ const retrievePosts = async () => {
     }
 }
 
+/**
+Get a single post by id
+@param id post id
+ */
+const getSinglePostById = async(id) => {
+    try {
+        const response = await fetch('/posts/' + id,
+            {
+                method: 'get',
+                headers: { "Content-Type": "application/json" },
+            })
+        const post = await response.json()
+        return post
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+/**
+Update a single post
+@param id post id
+@param updates a JSON object consisting of fields and values to update
+ */
+const updatePost = async(id, updates) => {
+    console.log(JSON.stringify(updates))
+    try {
+        const response = await fetch('/posts/' + id,
+            {
+                method: 'PATCH',
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(updates)
+            })
+        const post = await response.json()
+        return post
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+/**
+Delete a single post
+@param id post id
+ */
+const deletePost = async(id) => {
+    try {
+        const response = await fetch('/posts/' + id,
+            {
+                method: 'delete',
+                headers: { "Content-Type": "application/json" },
+            })
+        const post = await response.json()
+        return post
+    }
+    catch (e) {
+        console.log(e)
+    }
+}
+
+/**
+Load the full post to a page
+ */
 const loadPostsToPage = async () => {
     //EXPLANATION:
     //loadPostsToPage is an asynchronous function, so it will return a promise
@@ -32,19 +102,11 @@ const loadPostsToPage = async () => {
     console.log(posts)
 }
 
+/**
+Load posts to index page
+TODO: load only the most recent 10-15 posts
+ */
 const loadIndex = async () => {
-    //EXPLANATION:
-    //loadPostsToPage is an asynchronous function, so it will return a promise
-    //a promise specifies that the asynchronous function will return a needed value sometime in the future
-
-    //Read more about aysnchronous JS, Promises, and the async-await patterns here: 
-    // https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Asynchronous/Introducing
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-    //(most of the time, so we don't need to write promises on our own). But we need to know
-    //when a function returns a promise so we can handle it appropriately.
-
-    //in an async function, we use await to wait for the result of another async function
-    //before continuing execution
     const posts = await retrievePosts()
 
     //Use posts to update HTML below...
--- routers/postRoutes.js
+++ routers/postRoutes.js
@@ -13,12 +13,6 @@ router.post('/posts', async (req,res) => {
     }
 })
 
-// Find post by title
-/*
-case doesnt matter - use name for 
-for now exact match
-use findOne
-*/
 router.get('/posts', async (req, res) => {
     try {
         // console.log(req.body)
@@ -33,6 +27,21 @@ router.get('/posts', async (req, res) => {
     }
 })
 
+router.get('/posts/:id', async (req, res) => {
+    console.log("Calling get")
+    try {
+        // console.log(req.body)
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).send('Post not found!')
+        }
+        res.status(200).send(post)
+    }
+    catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 /*
 TODO:
 Field validation is not needed bc we'll use a form.
@@ -41,10 +50,14 @@ After posting, how do we get every display to update with new post?
 maybe autorefresh in background?
 */
 router.patch('/posts/:id', async (req, res) => {
+    console.log("Calling patch")
     const updates = Object.keys(req.body)
+    console.log(updates)
     try {
         // This is the traditional method of updating in mongoose
+        console.log(req.params.id)
         const post = await Post.findById(req.params.id)
+        
 
         //apply each update, then save. It already does validation for us
         updates.forEach((update) => {
